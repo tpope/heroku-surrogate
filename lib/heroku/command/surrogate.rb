e006091ac7f914f66978c6bd52a217d7a07ff119
Andre Arko <andre@arko.net>
2017-06-27 23:20:46 -0700

    Use the Heroku platform API
    The Heroku API exposed to Ruby plugins has been deprecated, and is
    starting to suffer brownouts. As a result, the only way to continue
    using this plugin is to fetch the needed information from the new API.
    Unfortunately, the new API spreads out the three pieces of information
    that we need across three separate API responses, which is... less than
    ideal. So this plugin is slower than it used to be. But at least it
    works again.

--- lib/heroku/command/surrogate.rb
+++ lib/heroku/command/surrogate.rb
@@ -1,3 +1,7 @@
+require 'net/http'
+require 'stringio'
+require 'json'
+
 # run commands locally using the remote environment
 class Heroku::Command::Surrogate < Heroku::Command::Base
 
@@ -20,11 +24,7 @@ class Heroku::Command::Surrogate < Heroku::Command::Base
   #   $ heroku surrogate PORT=3000 web
   #   $ heroku surrogate $SHELL
   def index
-    if options[:release]
-      release = api.get_release(app, options[:release]).body
-    else
-      release = api.get_releases(app).body.last
-    end
+    release = get_release(app, options[:release])
 
     vars = release['env'].inject({}) do |m, (k, v)|
       m.update(k => v.to_s)
@@ -76,5 +76,49 @@ class Heroku::Command::Surrogate < Heroku::Command::Base
       |testrb\b|rspec\b|cucumber\b
     )
   }x
+
+private
+
+  def get_release(app, release_id = nil)
+    release = get_release_info(app, release_id)
+    env     = Thread.new { get_config_vars(app, release['id']) }
+    pstable = Thread.new { get_pstable(app, release['slug']['id']) }
+
+    release.merge('env' => env.value, 'pstable' => pstable.value)
+  end
+
+  def get_config_vars(app, release_id)
+    api_request("/apps/#{URI.escape(app)}/releases/#{URI.escape(release_id)}/config-vars")
+  end
+
+  def get_pstable(app, slug_id)
+    api_request("/apps/#{URI.escape(app)}/slugs/#{URI.escape(slug_id)}").fetch("process_types", {})
+  end
+
+  def get_release_info(app, release_id = nil)
+    api_path = "/apps/#{URI.escape(app)}/releases"
+    api_path << "/#{URI.escape(release_id)}" if release_id
+    response_data = api_request(api_path)
+    release_id ? response_data : response_data.last
+  end
+
+  def api_request(api_path)
+    uri = URI("https://api.heroku.com") + api_path
+    req = Net::HTTP::Get.new uri
+    req['Accept'] = "application/vnd.heroku+json; version=3"
+    req['Authorization'] = "Bearer #{Heroku::Auth.password}"
+    req['Range'] = "id ..; order=desc,max=1;"
+
+    res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+      http.request(req)
+    end
+
+    response_json = res.body
+    if response_json.bytes[0..1] == [31, 139] # gzip magic bytes
+      io = StringIO.new(response_json, "rb")
+      response_json = Zlib::GzipReader.new(io).read
+    end
+    response_data = JSON.parse(response_json)
+  end
 
 end
